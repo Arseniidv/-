56d71586bd53a40d02a64fb7cbbc62168055f35c
Arseniidv <111341627+Arseniidv@users.noreply.github.com>
2025-04-30 20:11:36 +0300

    вывод чисел из диапазона 1-10

---
+++ ввод чисел в диапозоне 1-10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введите первое число (0-10): ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число (0-10): ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10)
+            {
+                Console.WriteLine("Числа должны быть от 0 до 10! Попробуйте снова.\n");
+            }
+            else
+            {
+                Console.WriteLine($"Результат умножения: {num1 * num2}");
+                break;
+            }
+        }
+    }
+}
