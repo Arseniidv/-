6151537a4cc6c4c5944bb9feff6de6eaa66cb738
Arseniidv <111341627+Arseniidv@users.noreply.github.com>
2025-04-30 20:09:59 +0300

    задания с вкладами (1-е и 2-е)

---
+++ задание с вкладами 1.cs
@@ -0,0 +1,22 @@
+﻿//задание с вкладавами 1
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите сумму вклада: ");
+        decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Введите количество месяцев: ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < months; i++)
+        {
+            amount *= 1.07m; // Увеличиваем сумму на 7%
+        }
+
+        Console.WriteLine($"Сумма через {months} месяцев: {amount:F2}");
+    }
+}
+
---
+++ задание с вкладами 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите сумму вклада: ");
+        decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Введите количество месяцев: ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        int currentMonth = 0;
+        while (currentMonth < months)
+        {
+            amount *= 1.07m;
+            currentMonth++;
+        }
+
+        Console.WriteLine($"Сумма через {months} месяцев: {amount:F2}");
+    }
+}
